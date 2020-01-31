5cd895a88b808bec485114bd6198ee899f116b80
Damon Self <damongself@gmail.com>
2020-01-31 12:39:16 -0500

    Implements CRUD for Owners
    Migrates the DB table for Owners. It has columns for a string for name
    and an integer for age.
    
    Creates 5 controller actions for Owner which consist of index, show,
    create, update, destroy. All actions are currently functioning properly.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_162224) do
+ActiveRecord::Schema.define(version: 2020_01_31_172705) do
 
   create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "owners", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
